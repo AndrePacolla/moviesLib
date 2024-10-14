8fbfa73f9954bb23aa95978d8b1e6a44a3c1a94b
AndrePacolla <andreluizzpacolla@outlook.com>
2024-10-14 14:04:36 -0300

    teste routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
-import App from './App.jsx'
+import App from './App'
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Movie from './pages/Movie';
+
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
