1f582d24645aaadce9645a69c1b5d8d971dd7a54
AndrePacolla <andreluizzpacolla@outlook.com>
2024-11-02 16:10:25 -0300

    renderizando title top filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,23 +8,28 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const Home = () => {
     const [topMovies, setTopMovies ] = useState([]);
 
-    const getTopRateMovies = async (url) => {
+    const getTopRatedMovies = async (url) => {
 
-        const res = await fecth(url);
+        const res = await fetch(url);
         const data = await res.json();
 
-        console.log(data)
+        setTopMovies(data.results);
     };
 
     useEffect(() => {
         const topRateUrl =`${moviesURL}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRateUrl);
+
 
     }, [])
 
    
     return (
         <div>
-            HOME
+            {topMovies && topMovies.map((movie) => 
+                <p> {movie.title}</p>) 
+            } 
         </div>
     ) 
 }
