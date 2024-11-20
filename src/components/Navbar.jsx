319a2287a04c2ca7e3177512b7dffbcfeb2139ea
AndrePacolla <andreluizzpacolla@outlook.com>
2024-11-20 18:38:38 -0300

    routes navigator

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import {Link, useNavigate } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import "./Navbar.css";
 
 const Navbar = () => { 
-    return(
+
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      navigate(`/search?q=${search}`);
+      setSearch("");
+    }
+
+
+
+
+    return(  
       <nav id="navbar">
         <h2>
           <Link to="/"> <BiCameraMovie/> MoviesLib </Link>
         </h2>
-        <form >
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Busque um filme"/>
-          <button type="submit"> <BiSearchAlt2/> </button>
+          <button type="submit">
+             <BiSearchAlt2/>
+           </button>
         </form>
       </nav>
     );
