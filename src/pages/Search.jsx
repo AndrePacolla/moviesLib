e72c1c85d0c5b6dc442553e2be8e74fbf321d1fc
AndrePacolla <andreluizzpacolla@outlook.com>
2024-11-22 17:53:45 -0300

    desenvolvendo search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,28 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
+const searchUrl = import.meta.env.VITE_SERACH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import "./MoviesGrid.css"
+
 const Search = () => {
+    
+
     return (
-        <div>
-            SEARCH
-        </div>
+        <div className="container">
+
+        <h2 className="title">
+            Resultados para : <span className="query-text">{query}</span>
+        </h2>
+
+          <div className="movies-container">
+
+              {topMovies.length === 0 && <p>Carregando ...</p>}
+              {topMovies.length > 0 && topMovies.map((movie) => <MovieCard movie={movie}/>) 
+              } 
+          </div>
+      </div>
     ) 
 }
 
