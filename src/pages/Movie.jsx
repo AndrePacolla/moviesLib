b560fab54fe7987b2c5a60aa7d97abc7006f0860
AndrePacolla <andreluizzpacolla@outlook.com>
2024-12-04 17:11:09 -0300

    movie-page return inf

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -31,6 +31,16 @@ const Movie = () => {
         getMovie(movieUrl) 
         
     }, [])
+
+
+    const formatCurrency = (number) => {
+
+        return number.toLocaleString("en-US",{
+            style: "currency",
+            currency: "USD"
+        });
+
+    }
     
     return ( 
      <div className="movie-page">
@@ -40,10 +50,30 @@ const Movie = () => {
               <p className="tagline">{movie.tagline}</p>
               <div className="info">
                 <h3>
-                    <BsWallet2/>Orçamento
+                    <BsWallet2/>Orçamento:
                 </h3>
                 <p>{movie.budget}</p>
+              </div>
 
+              <div className="info">
+                <h3>
+                    <BsGraphUp/> Receita:
+                </h3>
+                <p>{formatCurrency(movie.revenue)}</p>
+              </div>
+              
+              <div className="info">
+                <h3>
+                    <BsHourglassSplit/> Duração:
+                </h3>
+                <p>{movie.runtime} minutos</p>
+              </div>
+              
+              <div className="info description">
+                <h3>
+                    <BsFillFileEarmarkTextFill/> Descrição:
+                </h3>
+                <p>{movie.overview}</p>
               </div>
           </>
         )}
